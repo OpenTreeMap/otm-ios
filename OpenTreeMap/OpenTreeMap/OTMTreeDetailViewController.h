336001dc7189249e056344809395ace304a20d4f
Adam Hinz <ahinz@azavea.com>
2012-03-14 10:37:54 -0400

    detail views

---
+++ OpenTreeMap/OpenTreeMap/OTMTreeDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  OTMTreeDetailViewController.h
+//  OpenTreeMap
+//
+//  Created by Adam Hinz on 3/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTMTreeDetailViewController : UIViewController
+
+@end
---
+++ OpenTreeMap/OpenTreeMap/OTMTreeDetailViewController.m
@@ -0,0 +1,44 @@
+//
+//  OTMTreeDetailViewController.m
+//  OpenTreeMap
+//
+//  Created by Adam Hinz on 3/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "OTMTreeDetailViewController.h"
+
+@interface OTMTreeDetailViewController ()
+
+@end
+
+@implementation OTMTreeDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
