d6b82ca8c909c30bec5dfa15966d5d6615a071d4
Adam Hinz <ahinz@azavea.com>
2012-03-28 17:27:31 -0400

    refactor and update with review comments

---
+++ OpenTreeMap/OpenTreeMap/AZUser.h
@@ -0,0 +1,13 @@
+//
+//  AZUser.h
+//  OpenTreeMap
+//
+//  Created by Adam Hinz on 3/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AZUser : NSObject
+
+@end
---
+++ OpenTreeMap/OpenTreeMap/AZUser.m
@@ -0,0 +1,13 @@
+//
+//  AZUser.m
+//  OpenTreeMap
+//
+//  Created by Adam Hinz on 3/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AZUser.h"
+
+@implementation AZUser
+
+@end
