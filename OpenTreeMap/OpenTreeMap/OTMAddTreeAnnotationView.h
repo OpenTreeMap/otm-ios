7ce450de2fbf97c1285301c22e7b03ffdaae6856
Justin Walgran <jwalgran@azavea.com>
2012-04-23 09:53:22 -0400

    Update add tree annotation to use a drag offset
    The annotation has a large 'handle' around it so
    it can be easily dragged without the user' finger
    obscuring the actual point. This code calculates
    the offset from center when the user touches the
    annotation and uses it to corruptly reposition the
    annotation while it is being dragged.

--- OpenTreeMap/OpenTreeMap/OTMAddTreeAnnotationView.h
+++ OpenTreeMap/OpenTreeMap/OTMAddTreeAnnotationView.h
@@ -10,9 +10,31 @@
 #import <MapKit/MapKit.h>
 
 @interface OTMAddTreeAnnotationView : MKAnnotationView {
+    /**
+     A flag to indicate whether this annotation is in the middle of being dragged
+     */
     BOOL isMoving;
-}
+
+    /**
+     The horizontal distance in pixels from the center of the annotation to the point
+     at which the user touched and held the annotation. This annotation has a large
+     'handle' so that it can be dragged without the center being obscured by the user's
+     finger.
+     */
+    float touchXOffset;
     
+    /**
+     The vertical distance in pixels from the center of the annotation to the point
+     at which the user touched and held the annotation. This annotation has a large
+     'handle' so that it can be dragged without the center being obscured by the user's
+     finger.
+     */
+    float touchYOffset;
+}
+
+/**
+ A reference to the MKMapView to which this annotation has been added
+ */
 @property (nonatomic, strong) MKMapView *mapView;
 
 @end
--- OpenTreeMap/OpenTreeMap/OTMAddTreeAnnotationView.m
+++ OpenTreeMap/OpenTreeMap/OTMAddTreeAnnotationView.m
@@ -17,23 +17,59 @@
     
 	if (self) {
 		self.image = [UIImage imageNamed:@"handle_icon"];
+        [self clearTouch];
 	}
 	return self;
 }
 
+/**
+ Set the touchXOffset and touchYOffset members by calculating the distance from the
+ the touch to the center of the annotation view.
+ */
+- (void)setOffsetsFromTouch:(UITouch *)touch
+{
+    CGPoint point = [touch locationInView:self];
+    touchXOffset = point.x - self.bounds.size.width / 2.0;
+    touchYOffset = point.y - self.bounds.size.height / 2.0;
+}
+
+/**
+ Set the coordinate of the MKPointAnnotation by adding the touchXOffset and
+ touchYOffset to the touch and then using the MKMapView to translate the pixel
+ location to a map coordinate.
+ */
+- (void)updateCoordinateFromTouch:(UITouch *)touch
+{
+    CGPoint point = [touch locationInView:self.superview];
+    point.x -= touchXOffset;
+    point.y -= touchYOffset;
+    CLLocationCoordinate2D newCoordinate = [mapView convertPoint:point
+                                            toCoordinateFromView:self.superview];
+    self.annotation.coordinate = newCoordinate;
+}
+
+/**
+ Reset the touch tracking member variables to an initial state.
+ */
+- (void)clearTouch
+{
+    isMoving = NO;
+    touchXOffset = 0.0;
+    touchYOffset = 0.0;
+}
+
+#pragma mark UIResponder delgate methods
+
 -(void)touchesBegan:(NSSet*)touches withEvent:(UIEvent*)event
 {
     isMoving = YES;
+    [self setOffsetsFromTouch:[[event allTouches] anyObject]];
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event 
 {
     if (isMoving) {
-        UITouch *touch = [[event allTouches] anyObject];
-        CGPoint point = [touch locationInView:self.superview];
-        CLLocationCoordinate2D newCoordinate = [mapView convertPoint:point 
-                                            toCoordinateFromView:self.superview]; 
-        self.annotation.coordinate = newCoordinate;
+        [self updateCoordinateFromTouch:[[event allTouches] anyObject]];
     } else {
         [super touchesMoved:touches withEvent:event];
     }
@@ -41,12 +77,12 @@
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    isMoving = NO;
+    [self clearTouch];
 }
 
 - (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    isMoving = NO;
+    [self clearTouch];
 }
 
 @end
