OpenTreeMap/otm-ios

commit 215a6183308361f6f270e56aa71689af802479d9

Update detail view with configurable section headers
An enhancement request from Kelaine. She thought,
and Deb and I agreed, that the it was not clear
enough what the fields at the bottom of the
tree detail view were for. Labeling the section 
"Eco Benefits" removes any ambiguity.

OpenTreeMap/OpenTreeMap/OTMTreeDetailViewController.m
@@ -538,8 +538,19 @@
     return [[curFields objectAtIndex:section] count];
 }
 
-- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    return nil;
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    NSString *title = nil;
+
+    NSDictionary *fieldSectionDict = [[[OTMEnvironment sharedEnvironment] fieldSections] objectAtIndex:section];
+    if (fieldSectionDict) {
+        if ([fieldSectionDict objectForKey:@"label"]) {
+            if (![(NSString *)[fieldSectionDict objectForKey:@"label"] isEqualToString:@""]) {
+                title = [fieldSectionDict objectForKey:@"label"];
+            }
+        }
+    }
+    return title;
 }
 
 - (void)tableView:(UITableView *)tblView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {

OpenTreeMap/OpenTreeMap/Implementation.plist.base
@@ -45,6 +45,33 @@
 	<string>OTMRangeFilter</string>
       </dict>
     </array>
+    <key>fieldSections</key>
+	<array>
+		<dict>
+			<key>label</key>
+			<string></string>
+			<key>name</key>
+			<string>Map Detail</string>
+		</dict>
+		<dict>
+			<key>label</key>
+			<string></string>
+			<key>name</key>
+			<string>Tree Measurements</string>
+		</dict>
+		<dict>
+			<key>label</key>
+			<string></string>
+			<key>name</key>
+			<string>Plot Details</string>
+		</dict>
+		<dict>
+			<key>label</key>
+			<string>Eco Benefits</string>
+			<key>name</key>
+			<string>Eco Benefits</string>
+		</dict>
+	</array>
     <key>fields</key>
     <array>
       <array>

OpenTreeMap/OpenTreeMap/OTMEnvironment.h
@@ -102,6 +102,7 @@
 @property (nonatomic, strong) UIImage *buttonImage;
 @property (nonatomic, strong) UIColor *buttonTextColor;
 @property (nonatomic, assign) BOOL pendingActive;
+@property (nonatomic, strong) NSArray* fieldSections;
 @property (nonatomic, strong) NSDictionary* fields;
 @property (nonatomic, strong) NSArray* filts;
 @property (nonatomic, assign) BOOL useOtmGeocoder;

OpenTreeMap/OpenTreeMap/OTMEnvironment.m
@@ -22,7 +22,7 @@
 
 @implementation OTMEnvironment
 
-@synthesize urlCacheName, urlCacheQueueMaxContentLength, urlCacheInvalidationAgeInSeconds, mapViewInitialCoordinateRegion, mapViewSearchZoomCoordinateSpan, searchSuffix, locationSearchTimeoutInSeconds, mapViewTitle, api, baseURL, apiKey, choices, fieldKeys, viewBackgroundColor, navBarTintColor, buttonImage, buttonTextColor, filters, pendingActive, fields, filts, useOtmGeocoder, searchRegionRadiusInMeters;
+@synthesize urlCacheName, urlCacheQueueMaxContentLength, urlCacheInvalidationAgeInSeconds, mapViewInitialCoordinateRegion, mapViewSearchZoomCoordinateSpan, searchSuffix, locationSearchTimeoutInSeconds, mapViewTitle, api, baseURL, apiKey, choices, fieldKeys, viewBackgroundColor, navBarTintColor, buttonImage, buttonTextColor, filters, pendingActive, fieldSections, fields, filts, useOtmGeocoder, searchRegionRadiusInMeters;
 
 + (id)sharedEnvironment
 {
@@ -68,6 +68,7 @@
     
     self.apiKey = [implementation valueForKey:@"APIKey"];
     
+    fieldSections = [implementation objectForKey:@"fieldSections"];
     fields = [implementation objectForKey:@"fields"];
     filts = [implementation objectForKey:@"filters"];
 

commit 01274853177a6a1b2c0e8b7b2fcd77c7f069829e

Fix crashing bug when editing tree
I introduced this bug in 215a6183 when I added the
section header labels to the tree detail view.
When the view goes into edit mode, the number of
sections in the table changes and my
titleForHeaderInSection method was causing index
out of bounds exceptions.

OpenTreeMap/OpenTreeMap/OTMTreeDetailViewController.m
@@ -540,17 +540,22 @@
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
-    NSString *title = nil;
-
-    NSDictionary *fieldSectionDict = [[[OTMEnvironment sharedEnvironment] fieldSections] objectAtIndex:section];
-    if (fieldSectionDict) {
-        if ([fieldSectionDict objectForKey:@"label"]) {
-            if (![(NSString *)[fieldSectionDict objectForKey:@"label"] isEqualToString:@""]) {
-                title = [fieldSectionDict objectForKey:@"label"];
+    if (editMode) {
+        // The view controller rearranges all the sections in the table view
+        // when it is in edit mode, so the section labels no longer apply.
+        return nil;
+    } else {
+        NSString *title = nil;
+        NSDictionary *fieldSectionDict = [[[OTMEnvironment sharedEnvironment] fieldSections] objectAtIndex:section];
+        if (fieldSectionDict) {
+            if ([fieldSectionDict objectForKey:@"label"]) {
+                if (![(NSString *)[fieldSectionDict objectForKey:@"label"] isEqualToString:@""]) {
+                    title = [fieldSectionDict objectForKey:@"label"];
+                }
             }
         }
+        return title;
     }
-    return title;
 }
 
 - (void)tableView:(UITableView *)tblView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {