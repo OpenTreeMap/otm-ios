42406a1a3fe8b1754015fb5d35c21e78eb43f1a1
Adam Hinz <ahinz@azavea.com>
2012-09-05 15:08:55 -0400

    Fix memory exception when root VC was out of scope
    When a seque is "swapped" out as the root VC, nothing maintains
    a reference to the view controller.
    
    Since we only use this in one place (the initial delay), a
    forced retain was inserted.
    
    If there is a better way to chagne the root vc for the app, it
    should supersede this commit

--- OpenTreeMap/OpenTreeMap/OTMSwapSegue.m
+++ OpenTreeMap/OpenTreeMap/OTMSwapSegue.m
@@ -14,8 +14,10 @@
     UIView *sView = [[self sourceViewController] view];
     CGRect sFrame = sView.frame;
     UIView *pView = [sView superview];
-    [sView removeFromSuperview];
+    [sView removeFromSuperview];    
     
+    // Force a retain on the destinationViewController
+    CFRetain((__bridge CFTypeRef)[self destinationViewController]);
     UIView *dView = [[self destinationViewController] view];
     dView.frame = sFrame;
     [pView addSubview:dView];
