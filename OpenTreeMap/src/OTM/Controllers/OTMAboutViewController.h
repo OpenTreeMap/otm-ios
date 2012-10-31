fa88420693a441c6cfd894b0114ed6b50e673e96
Justin Walgran <jwalgran@azavea.com>
2012-10-31 13:13:07 -0400

    Add support for loading about.html
    This commit adds support for loading a custom
    about.html page in a UIWebView on the About tab,
    allowing for an arbitrary amount of scrolling text
    and links.
    
    The project references 3 new files that _must_ be
    included in every skin to ensure that the project
    builds correctly.
    
      - about.html
      - about_header@2x.png
      - about_footer@2x.png
    
    If the about.html file is less than 64 bytes
    (to allow for an explanatory comment) the
    UIWebView is hidden and about_bg.png is shown
    as in previous version of the app.

---
+++ OpenTreeMap/src/OTM/Controllers/OTMAboutViewController.h
@@ -0,0 +1,16 @@
+//
+//  OTMAboutViewController.h
+//  OpenTreeMap
+//
+//  Created by Justin Walgran on 10/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTMAboutViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UIImageView *backgroundImageView;
+@property (nonatomic, strong) IBOutlet UIWebView *webView;
+
+@end
---
+++ OpenTreeMap/src/OTM/Controllers/OTMAboutViewController.m
@@ -0,0 +1,88 @@
+// This file is part of the OpenTreeMap code.
+//
+// OpenTreeMap is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenTreeMap is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenTreeMap.  If not, see <http://www.gnu.org/licenses/>.
+
+#import "OTMAboutViewController.h"
+
+@interface OTMAboutViewController ()
+
+@end
+
+@implementation OTMAboutViewController
+
+@synthesize backgroundImageView, webView;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    backgroundImageView.image = [UIImage imageNamed:@"about_bg"];
+
+    webView.opaque = YES;
+    
+    NSString *aboutPagePath = [[NSBundle mainBundle] pathForResource:@"about" ofType:@"html"];
+    if (aboutPagePath) {
+        NSDictionary *fileAttributes = [[NSFileManager defaultManager] attributesOfItemAtPath:aboutPagePath error:nil];
+        long long fileSize = [[fileAttributes valueForKey:@"NSFileSize"] longLongValue];
+        // We are checking file size here because the some skins will use an HTML page and some
+        // will just use the static background image. Since the HTML is referenced by the project,
+        // it cannot be omitted, so skins that do not use about.html will just have an empty
+        // file, or one with a short comment.
+        if (fileSize > 64) {
+            NSURL *url = [NSURL fileURLWithPath:aboutPagePath];
+            NSString *html = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:nil];
+            
+            [webView loadHTMLString:html baseURL:[url URLByDeletingLastPathComponent]];
+        } else {
+            NSLog(@"about.html was less than 64 bytes in size. Hiding the about UIWebView.");
+            webView.hidden = YES;
+        }
+    } else {
+        NSLog(@"about.html was not found in the bundle. Hiding the about UIWebView.");
+        webView.hidden = YES;
+    }
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark UIWebView Delegate methods
+
+-(BOOL) webView:(UIWebView *)inWeb shouldStartLoadWithRequest:(NSURLRequest *)inRequest navigationType:(UIWebViewNavigationType)inType {
+    if ( inType == UIWebViewNavigationTypeLinkClicked ) {
+        [[UIApplication sharedApplication] openURL:[inRequest URL]];
+        return NO;
+    }
+    
+    return YES;
+}
+
+@end
