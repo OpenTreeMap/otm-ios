6b321631caa3dce7e14958767c3227ad041478a2
Laurence Liss <lliss@azavea.com>
2014-06-06 11:27:00 -0400

    Fixup comment linebreaks.

--- OpenTreeMap/src/OTM/OTMAppDelegate.m
+++ OpenTreeMap/src/OTM/OTMAppDelegate.m
@@ -46,8 +46,12 @@
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    // Sent when the application is about to move from active to inactive state.
+    // This can occur for certain types of temporary interruptions (such as an
+    // incoming phone call or SMS message) or when the user quits the
+    // application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down
+    // OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -62,7 +66,9 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    // Restart any tasks that were paused (or not yet started) while the
+    // application was inactive. If the application was previously in the
+    // background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
