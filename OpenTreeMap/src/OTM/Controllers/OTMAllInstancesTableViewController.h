9702b42b8ccac1283e29f9486c6cb6a349a40fcd
Laurence Liss <lliss@azavea.com>
2014-07-01 14:33:57 -0400

    Add new table view controller for all instances.
    This new controller calls the shared settings which updates the list of
    instances and then generates an alphabetically sorted list of table cells. The
    cells are added to the controller in a single section. The all maps view  has a
    delegate that will be set to the instance switcher controller so that controller
    can update perform the instance switch which avoids the instance switch logic in
    multiple places in the code.

---
+++ OpenTreeMap/src/OTM/Controllers/OTMAllInstancesTableViewController.h
@@ -0,0 +1,34 @@
+// This file is part of the OpenTreeMap code.
+//
+// OpenTreeMap is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenTreeMap is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenTreeMap.  If not, see <http://www.gnu.org/licenses/>.
+
+#import <UIKit/UIKit.h>
+
+@protocol OTMAllInstancesViewControllerDelegate;
+
+  @interface OTMAllInstancesTableViewController : UITableViewController <UISearchBarDelegate>
+
+  @property (weak, nonatomic) IBOutlet UISearchBar *search;
+  @property (nonatomic, weak) id<OTMAllInstancesViewControllerDelegate> delegate;
+  @property NSArray *sections;
+  @property NSDictionary *instances;
+  @property NSMutableArray *filteredInstances;
+  @property NSMutableDictionary *sectionDict;
+@end
+
+@protocol OTMAllInstancesViewControllerDelegate <NSObject>
+
+- (void)instanceDidUpdate:(OTMAllInstancesTableViewController *)controller
+          withInstanceUrl:(NSString *) instance;
+@end
